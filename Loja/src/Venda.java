1c163a45d36f1204460ebbcbe99f02beccae8254
aluno <aluno@DSN1251017054>
2022-05-09 09:06:49 -0300

    09/05

---
+++ Loja/src/Venda.java
@@ -0,0 +1,18 @@
+
+public class Venda {
+
+	public static void main(String[] args) {
+		double prod1 = 50 ;
+		double prod2 = 20 ;
+		double total;
+		double  desc ;
+		
+		total = prod1 + prod2;
+		desc = total * 0.85;//valor restante a porcentagem//
+		System.out.println("O Total da compra foi de  " + total);
+		System.out.println("Com desconto o valor ficará " + desc);
+		 
+
+	}
+
+}
