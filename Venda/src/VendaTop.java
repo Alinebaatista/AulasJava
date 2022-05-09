3f6626c1706da001dadee43bd6de0fcb2191d0bd
aluno <aluno@DSN1251017054>
2022-05-09 09:07:32 -0300

    09/05

---
+++ Venda/src/VendaTop.java
@@ -0,0 +1,26 @@
+
+public class VendaTop {
+
+	public static void main(String[] args) {
+		double prod1;
+		double prod2;
+		double desc;
+		double total;
+	
+		
+		System.out.println("Os produtos Disponiveis são: " );
+		System.out.println("Calça, Valor:R$60,00" );
+		System.out.println("Blusa, Valor:R$25,00" );
+		System.out.println("Short, Valor:R$45,00 ");
+		System.out.println("Vestido, Valor:R$49;99");
+		
+		if(prod1)("Calça") {
+		System.out.println(" PARA BENS ! Ao selecionar este produtos voce ganhou um desconto de 15%");
+			
+		}
+		
+		System.out.println("OS produtos selecionads foram: " +  prod1 "e" + prod2 "Ao total desta compra foi de:" + total);
+
+	}
+
+}
