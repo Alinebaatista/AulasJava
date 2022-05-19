207180a5fdd6868f44d52493d5318e0e756ea7e1
Alinebaatista <102964288+Alinebaatista@users.noreply.github.com>
2022-05-19 07:46:02 -0300

    Update Aplicacao.java

--- ClasseAbistrata2/src/Aplicacao.java
+++ ClasseAbistrata2/src/Aplicacao.java
@@ -1,5 +1,7 @@
 
 public class Aplicacao {
+//não emplementa codigos e valores, é um molde pra as subclasses (ela padroniza) sempre é uma super classe.força quem estendeu a colocar atributos e métodos.
+//A class abistrata pode ter metodos abstratos.
 
 	public static void main(String[] args) {
 
