263d4bb439a1801449034681e9718b35ea8cd642
Alinebaatista <102964288+Alinebaatista@users.noreply.github.com>
2022-05-19 07:41:55 -0300

    Update VendaTop.java

--- Venda/src/VendaTop.java
+++ Venda/src/VendaTop.java
@@ -8,14 +8,14 @@ public class VendaTop {
 		double total;
 	
 		
-		System.out.println("Os produtos Disponiveis s„o: " );
-		System.out.println("CalÁa, Valor:R$60,00" );
+		System.out.println("Os produtos Disponiveis s√£o: " );
+		System.out.println("Cal√ßa, Valor:R$60,00" );
 		System.out.println("Blusa, Valor:R$25,00" );
 		System.out.println("Short, Valor:R$45,00 ");
 		System.out.println("Vestido, Valor:R$49;99");
 		
-		if(prod1)("CalÁa") {
-		System.out.println(" PARA BENS ! Ao selecionar este produtos voce ganhou um desconto de 15%");
+		if(prod1)("Cal√ßa") {
+		System.out.println(" PARABENS !! Ao selecionar este produtos voce ganhou um desconto de 15%");
 			
 		}
 		
