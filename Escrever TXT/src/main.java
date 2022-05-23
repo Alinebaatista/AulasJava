cb7baf5a7bb8df63a5c60b3ae11644eb0cc72784
Alinebaatista <102964288+Alinebaatista@users.noreply.github.com>
2022-05-23 11:02:15 -0300

    Add files via upload
    23/05

---
+++ Escrever TXT/src/main.java
@@ -0,0 +1,20 @@
+       //Buffered
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class main {
+
+	public static void main(String[] args) throws IOException {
+
+		String s = "Texto para ser gravado no arquivo";
+
+		try(BufferedWriter escrever = new BufferedWriter(new FileWriter("saida.txt"))) {
+			
+			escrever.write(s);
+		}
+	}
+
+	}
+
+
